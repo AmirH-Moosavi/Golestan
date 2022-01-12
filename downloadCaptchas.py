88ae943bb7a4b80473b5614d22db316c56c14a39
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-01-12 14:28:44 +0100

    fetch captchas from golestan

---
+++ downloadCaptchas.py
@@ -0,0 +1,24 @@
+import requests, os, random
+
+DIRNAME = "Golestan-Captchas"
+if not os.path.exists(DIRNAME):
+	os.mkdir(DIRNAME)
+
+uniques = []
+numberOfImages = 30
+counter = 1
+while counter <= numberOfImages:
+	url = 'https://golestan.ikiu.ac.ir/Forms/AuthenticateUser/captcha.aspx'
+	r = requests.get(url, allow_redirects=True)
+
+	if r.status_code == 200:
+		contentLength = r.headers['content-length']
+		if contentLength not in uniques:
+			uniques.append(contentLength)
+			fileName = str(int(random.uniform(10000, 99999))) + '.gif'
+			with open(DIRNAME + '/' + fileName, 'wb') as f:
+				f.write(r.content)
+			f.close()
+
+			print(counter)
+			counter += 1
