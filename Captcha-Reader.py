5e0f698751697ca0d02a7b8c34ae0920720dea21
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-01-21 13:15:13 +0100

    clean image noises and make image b&w

--- Captcha-Reader.py
+++ Captcha-Reader.py
@@ -1,17 +1,35 @@
-from PIL import Image
+import cv2
 
-im = Image.open("captcha.gif")
-im = im.convert("P")
-im2 = Image.new("P",im.size,255)
-im = im.convert("P")
+IMG_DIR = 'Golestan-Captchas/'
+INPUT_IMAGE = IMG_DIR + '80861.gif'
 
-temp = {}
+def sharp_img(img):
+	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	blur_image = cv2.blur(gray_img, (4, 4))
+	gblur_img = cv2.GaussianBlur(blur_image, (0, 0), 6)
+	sharp_img = cv2.addWeighted(gray_img, 1.80, gblur_img, -0.60, 0)
+	sharp_not_img = cv2.bitwise_not(sharp_img);
+	retval, img_zeroone = cv2.threshold(sharp_not_img, 20, 255, cv2.THRESH_BINARY)
+	return img_zeroone
 
-for x in range(im.size[1]):
-    for y in range(im.size[0]):
-        pix = im.getpixel((y,x))
-        temp[pix] = pix
-        if pix == 220or pix == 227: # these are the numbers to get_
-            im2.putpixel((y,x),0)
+im = cv2.imread(INPUT_IMAGE)
+sim = sharp_img(im)
 
-im2.save("output.gif")
+def clear_img(img):
+	gray = cv2.rectangle(img, (45, 45), (139, 49), (0,0,0), -1)
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
+	opening_img = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+
+	for height in range((opening_img.shape[0])):
+		for width in range((opening_img.shape[1])):
+			opening_img[height][width]
+			if opening_img[height][width] == 255:
+				opening_img[height][width] = 0
+			else:
+				opening_img[height][width] = 255
+	return opening_img
+
+cl_img = clear_img(sim)
+cv2.imshow("linesDetected", cl_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
