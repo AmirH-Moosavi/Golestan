2980e18e40a718923d4115787117429c485e6b63
Amir Mousavi <s.amirhosein.mn@gmail.com>
2022-01-21 13:43:11 +0330

    captcha reader updated with a more advanced captcha reader logic.

--- Captcha-Reader.py
+++ Captcha-Reader.py
@@ -1,9 +1,8 @@
 from PIL import Image
-
-im = Image.open("captcha.gif")
+import PIL
+im = Image.open(".\captcha.gif")
 im = im.convert("P")
 im2 = Image.new("P",im.size,255)
-im = im.convert("P")
 
 temp = {}
 
@@ -11,7 +10,7 @@ for x in range(im.size[1]):
     for y in range(im.size[0]):
         pix = im.getpixel((y,x))
         temp[pix] = pix
-        if pix == 220or pix == 227: # these are the numbers to get_
+        if pix == 220 or pix == 227: # these are the numbers to get_
             im2.putpixel((y,x),0)
 
 im2.save("output.gif")
